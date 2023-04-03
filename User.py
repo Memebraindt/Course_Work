c6a07586eb8786d92fbfe8e43be9f6a11124cc07
MemeBraindt <IlyaBraindt@yandex.by>
2023-04-03 13:43:47 +0300

    some changes

--- User.py
+++ User.py
@@ -11,25 +11,44 @@ def load_users():
         return pickle.load(file)
 
 
-def print_all(user_ids):
-    print("____Id____|___Name___|____Username____|____Last__Name____|")
-    for id_i in user_ids:
-        print(user_ids[id_i])
+def print_all(users):
+    s1 = "Id"
+    s = f"{s1:_^10}|"
+    s1 = "Name"
+    s += f"{s1:_>10}|"
+    s1 = "Username"
+    s += f"{s1:_^18}|"
+    s1 = "Last_Name"
+    s += f"{s1:_^18}|"
+    print(s)
+    # print("____Id____|___Name___|_____Username_____|____Last__Name____|")
+    for user in users:
+        print(users[user])
 
 
 class User:
-    def __init__(self, user_id, first_name, last_name, username):
+    def __init__(self, user_id, first_name, username, last_name, real_first_name, real_middle_name, real_last_name, ):
         self.__user_id = user_id
         self.__first_name = first_name
-        self.__last_name = last_name
         self.__username = username
+        self.__last_name = last_name
+        self.__real_first_name = real_first_name
+        self.__real_middle_name = real_middle_name
+        self.__real_last_name = real_last_name
 
     def __str__(self):
         user_str = f"{self.__user_id: >10}|{self.__first_name: >10}|"
         if self.__username is not None:
-            user_str += f"{self.__username: >16}|"
+            user_str += f"{self.__username: >18}|"
+        else:
+            user_str += " " * 18 + "|"
+
         if self.__last_name is not None:
             user_str += f"{self.__last_name: >16}|"
+        else:
+            user_str += " " * 18 + "|"
+        # user_str += self.__real_first_name
+
         return user_str
 
     def get_name(self):
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ dp = Dispatcher(bot)
 # Создаём словарь и сохраняем в него пользователей из файла
 # users = {}
 users = load_users()
+# users2 = {}
 print(users)
 
 
@@ -23,19 +24,24 @@ async def start_command(message: types.Message):
     if message.from_user.id not in users:
         # Если пользователя нет, создаём новый объект пользователя и добавляем в словарь.
         user = User(message.from_user.id, message.from_user.first_name,
-                    message.from_user.last_name, message.from_user.username)
+                    message.from_user.username, message.from_user.last_name,
+                    "First_Name", "Middle_name", "Last_name")
         users[message.from_user.id] = user
-        print(user)
         save_users(users)
     await bot.send_message(message.chat.id, "Приветствую")
+    await bot.send_message(message.chat.id, " Возможные команды: /help ...")
 
 
 @dp.message_handler(content_types=['text'])
 async def text(message: types.Message):
-    print(f"{users[message.from_user.id].get_name()}[id{message.from_user.id: >10}] напечатал: {message.text}")
-    await bot.send_message(message.chat.id, message.text)
-    if message.text == "/print_all":
+    mfi = message.from_user.id
+    mt = message.text
+    mci = message.chat.id
+    print(f"{users[mfi].get_name()}[id{mfi: >10}] напечатал: \n{mt}")
+    if mt == "/print_all":
         print_all(users)
+    else:
+        await bot.send_message(mci, mt)
 
 
 @dp.message_handler(content_types=['photo', 'video', 'sticker', 'document', 'audio', 'animation'])
