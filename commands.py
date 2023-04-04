abd6e6cfca11fb2d5a3a94223a3157a3d29d894d
MemeBraindt <IlyaBraindt@yandex.by>
2023-04-04 23:20:46 +0300

    added commands for different chats

---
+++ commands.py
@@ -0,0 +1,19 @@
+# import settings as st
+from User import *
+
+
+async def admin_commands(users, message_text):
+    if message_text == "/print_all":
+        print_all(users)
+
+    pass
+
+
+async def fif_commands():
+
+    pass
+
+
+async def bunker_commands():
+
+    pass
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
 import settings as st
+import commands as cm
 from User import *
 
 # bot -- экземпляр класса Бот
@@ -19,11 +20,13 @@ print_all(users)
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    if message.from_user.id not in users.keys():
-        user = User(str(message.from_user.id), message.from_user.first_name,
-                    message.from_user.username,  # message.from_user.last_name,
+    mf = message.from_user
+    mfi = int(mf.id)
+    if mfi not in users.keys():
+        user = User(mfi, mf.first_name,
+                    mf.username,  # message.from_user.last_name,
                     " " * 11, " " * 9, " " * 13)
-        users[message.from_user.id] = user
+        users[mfi] = user
         save_users(users)
     # else:
     #     Check_and_update()
@@ -33,15 +36,23 @@ async def start_command(message: types.Message):
 
 @dp.message_handler(content_types=['text'])
 async def text(message: types.Message):
-    print(message)
+    mci = message.chat.id
     mfi = int(message.from_user.id)
     mt = message.text
-    mci = message.chat.id
-    # print(f"{users[mfi].get_name()}[id{mfi: >10}] напечатал: \n{mt}")
-    if mt == "/print_all":
-        print_all(users)
+    is_adm_comm = (str(mci) == str(st.admin_id))
+    is_bunker_chat = (str(mci) == str(st.bunker_chat_id))
+    is_fif_chat = (str(mci) == str(st.fif_chat_id))
+    if is_adm_comm:
+        await cm.admin_commands(users, mt)
+    elif is_fif_chat:
+        await cm.fif_commands()
+    elif is_bunker_chat:
+        await cm.bunker_commands()
     else:
+        await bot.forward_message(st.admin_id, mci, message.message_id)
         await bot.send_message(mci, mt)
+    print(message)
+    # print(f"{users[mfi].get_name()}[id{mfi: >10}] напечатал: \n{mt}")
 
 
 @dp.message_handler(content_types=['photo', 'video', 'sticker', 'document', 'audio', 'animation'])
